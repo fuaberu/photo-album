9123e264a8daebde3d311309c4676547d5be107b
fuaberu <kevinfabel@gmail.com>
2021-11-26 00:27:30 -0300

    clean up

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useGetSearchPhotosQuery } from '../../services/pexels';
 import './galleryStyles.scss';
 
